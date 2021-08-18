92bd5937f02236f947e335bf0c5092701150e502
andyfil77 <89149704+andyfil77@users.noreply.github.com>
2021-08-18 22:55:16 +0600

    Add files via upload

--- main.c
+++ main.c
@@ -4,40 +4,40 @@
 
 #include <math.h>
 
-float quadro(a, b, c, &x1, &x2, &solutionNo)
+float quadro(double a, double b, double c, double *x1, double *x2, int *solutionNo)
 {
     /* ax^2 + bx + c = 0 */
-    double D, x1, x2;
+    double D;
     if (a == 0 && b != 0 && c != 0)
         {
-            x1 = -c / b;
-            solutionNo = 1;
+            *x1 = -c / b;
+            *solutionNo = 1;
         }
     else if ( a == 0 && b == 0 && c == 0)
         {
-            solutionNo = 3;
+            *solutionNo = 3;
         }
     else if ( a == 0 && b == 0 && c != 0)
         {
-            solutionNo = 0;
+            *solutionNo = 0;
         }
     else
         {
         D = b * b - 4 * a * c;
         if (D > 0)
         {
-            x1 = (-b + sqrt(D)) / (2 * a);
-			x2 = (-b - sqrt(D)) / (2 * a);
-			solutionNo = 2;
+            *x1 = (-b + sqrt(D)) / (2 * a);
+			*x2 = (-b - sqrt(D)) / (2 * a);
+			*solutionNo = 2;
         }
         else if (D == 0)
         {
-            x1 = -b / (2 * a);
-            solutionNo = 2;
+            *x1 = -b / (2 * a);
+            *solutionNo = 1;
         }
         else
         {
-            solutionNo = 0;
+            *solutionNo = 0;
         }
         }
     return 0;
@@ -45,10 +45,10 @@ float quadro(a, b, c, &x1, &x2, &solutionNo)
 
 int main()
 {
-    int a, b, c;
-    double x1, x2, solutionNo;
+    int a, b, c, solutionNo;
+    double x1, x2;
     scanf("%d %d %d", &a, &b, &c);
-    quadro(a,b,c);
+    quadro(a,b,c, &x1, &x2, &solutionNo);
     if (solutionNo == 0)
     {
         printf("the equation hasn't roots");
@@ -59,11 +59,11 @@ int main()
     }
     else if (solutionNo == 2)
     {
-        printf("the equation has one root: %.1f %.1f", x1, x2);
+        printf("the equation has two roots: %.1f %.1f", x1, x2);
     }
     else
     {
-        printf("the equation has infinity many roots")
+        printf("the equation has infinity many roots");
     }
     return 0;
 }
